d3d5b99ad98046f23b13d71da427d7d632a8d83e
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2024-04-29 22:02:20 +0200

    create files for login and Registration pages (just started)

---
+++ src/main/webapp/reactjs/src/components/Registration/Registration.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import "./Registration.css"
+
+function Registration() {
+    return (
+        <div className="registration">
+
+            <div className="registration-form">
+
+                <form className="registration-form--form">
+                    <h1 className="registration-header">Реєстрація</h1>
+                    <input className="registration-form-input"
+                           type="text"
+                           name="login"
+
+                    />
+                    <input className="registration-form-input"
+                           type="email"
+                           name="email"
+                    />
+
+                    <input className="registration-form-input"
+                           type="password"
+                           name="password"
+
+
+                    />
+                    <input className="registration-form-input"
+                           type="password"
+                           name="password"
+                    />
+
+                    <button className="registration-form-btn" type="submit">Зареєструватись
+                    </button>
+                    <p>Вже маєте акаунт? <button>Увійти</button></p>
+                </form>
+            </div>
+        </div>
+    );
+
+}
+
+export default Registration;
