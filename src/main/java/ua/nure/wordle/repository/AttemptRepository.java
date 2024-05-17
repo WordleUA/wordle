21a24e09bd14da228fc15f081dc1f374654bf939
Oksana Vykhryst <oksana.vykhryst@nure.ua>
2024-05-17 16:33:36 +0300

    WRDL-22 Deleted Attempts

--- src/main/java/ua/nure/wordle/repository/AttemptRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.nure.wordle.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.nure.wordle.entity.Attempt;
-
-@Repository
-public interface AttemptRepository extends JpaRepository<Attempt, Integer> {
-}
