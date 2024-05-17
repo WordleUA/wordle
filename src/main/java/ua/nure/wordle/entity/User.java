d058d61c5a85034e06ec9166d968e9593d6cb656
Oryna Kasapova <oryna.kasapova@nure.ua>
2024-05-17 15:54:49 +0300

    WRDL-22 Changed User entity

--- src/main/java/ua/nure/wordle/entity/User.java
+++ src/main/java/ua/nure/wordle/entity/User.java
@@ -22,7 +22,6 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(name = "username", nullable = false, length = 45)
     private String username;
 
