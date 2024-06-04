dce61168fe074ad096362ddc75631afb67b56832
sofiakolokolcheva <110667629+Sofikoshka@users.noreply.github.com>
2024-06-04 22:28:10 +0200

    WRDL-142 implemented navbar for administrator

---
+++ src/main/webapp/reactjs/src/components/Navbar/NavbarAdmin.jsx
@@ -0,0 +1,35 @@
+
+import "./NavbarClient/NavbarClient.css"
+
+import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router";
+
+function NavbarClient () {
+    const navigate = useNavigate();
+
+
+    return (
+        <nav className="nav">
+            <div className="container">
+                <div className="nav-row">
+                    <NavLink to="/howToPlay">
+                        <h3 className="logo-navbar">WORLDE UA</h3>
+                    </NavLink>
+
+                    <ul className="nav-list">
+                        <NavLink to="/clientCabinet" className="nav-list__item">Особистий кабінет</NavLink>
+                        <NavLink to='/administration' className="nav-list__item">Список користувачів</NavLink>
+                        <NavLink to="/dictateWord" className="nav-list__item" style={{ color: '#519341' }}>Почати гру</NavLink>
+                        <a className="nav-list__item">Вихід</a>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    );
+
+}
+export default NavbarClient;
+
+
+
