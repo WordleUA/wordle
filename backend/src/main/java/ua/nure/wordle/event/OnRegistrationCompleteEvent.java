bae6f934b0e69a9adc8af978fbac08a8d5e46e33
Oksana Vykhryst <oksana.vykhryst@nure.ua>
2024-06-09 21:36:44 +0300

    WRDL-159 Changed app url

--- backend/src/main/java/ua/nure/wordle/event/OnRegistrationCompleteEvent.java
+++ backend/src/main/java/ua/nure/wordle/event/OnRegistrationCompleteEvent.java
@@ -2,19 +2,19 @@ package ua.nure.wordle.event;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEvent;
 import ua.nure.wordle.entity.User;
 
 @Getter
 @Setter
 public class OnRegistrationCompleteEvent extends ApplicationEvent {
+    @Value("${wordle.app.url}")
     private String appUrl;
     private User user;
 
     public OnRegistrationCompleteEvent(User user) {
         super(user);
         this.user = user;
-        this.appUrl = "https://wordle-4fel.onrender.com";
-//        this.appUrl = "http://localhost:3000";
     }
 }
