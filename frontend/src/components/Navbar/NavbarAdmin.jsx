eaf1a7b12995578f43091055a95817db125a2b1e
sofiakolokolcheva <110667629+Sofikoshka@users.noreply.github.com>
2024-06-14 18:51:42 +0200

    WRDL-195 edited admin navbar

--- frontend/src/components/Navbar/NavbarAdmin.jsx
+++ frontend/src/components/Navbar/NavbarAdmin.jsx
@@ -25,7 +25,7 @@ function NavbarClient () {
 
                     <ul className="nav-list">
                         <NavLink to="/clientCabinet" className="nav-list__item">Особистий кабінет</NavLink>
-                        <NavLink to='/generalStatistic' className="nav-list__item">Статистика</NavLink>
+                        <NavLink to='/generalStatistic' className="nav-list__item">Рейтинг гравців</NavLink>
                         <NavLink to='/administration' className="nav-list__item">Адміністрування</NavLink>
                         <NavLink to="/dictateWord" className="nav-list__item" style={{ color: '#519341' }}>Почати гру</NavLink>
                         <a onClick={logout} className="nav-list__item">Вихід</a>
